b5d9e2e1b17bf067eab80d1e55fd7315687735de
Haraprasad Evince <haraprasad@evincedev.com>
2026-09-23 23:36:50 +0530

    added force dynamic to stop caching

--- portfolio/app/page.tsx
+++ portfolio/app/page.tsx
@@ -4,6 +4,8 @@ import SectionWrapper from "./components/SectionWrapper";
 import { PortfolioContent } from "./type";
 import { getContent } from "@/lib/content";
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   const content: PortfolioContent = await getContent();
 
